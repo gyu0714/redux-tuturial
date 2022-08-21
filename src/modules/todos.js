06ade46e0ced2968450aaef338b154cdc3f05ea8
gyu0714 <rlaaalsrb3472@gmail.com>
2022-08-21 16:55:23 +0900

    initial commit

---
+++ src/modules/todos.js
@@ -0,0 +1,61 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const CHANGE_INPUT = 'todos/CHANGE_INPUT';
+const INSERT = 'todos/INSERT';
+const TOGGLE = 'todos/TOGGLE';
+const REMOVE = 'todos/REMOVE';
+
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: '리덕스 기초',
+      done: true,
+    },
+    {
+      id: 2,
+      text: '리액트와 리덕스',
+      done: false,
+    },
+  ],
+};
+
+export const changeInput = createAction(CHANGE_INPUT, (input) => input);
+
+let id = 3;
+export const insert = createAction(INSERT, (text) => ({
+  id: id++,
+  text,
+  done: false,
+}));
+
+export const toggle = createAction(TOGGLE, (id) => id);
+
+export const remove = createAction(REMOVE, (id) => id);
+
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({
+      ...state,
+      input,
+    }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.don } : todo,
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    }),
+  },
+  initialState,
+);
+
+export default todos;
